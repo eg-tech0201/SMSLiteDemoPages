188cef13657968d501e3834c2cff82a15eeee096
Esha G <eshagtech@yahoo.com>
2026-06-11 18:45:13 -0400

    Fix Blazor JS interop and ELMA auth defaults

--- sms_lite/External.ELMA.Client/Configuration/ElmaClientOptions.cs
+++ sms_lite/External.ELMA.Client/Configuration/ElmaClientOptions.cs
@@ -8,7 +8,7 @@ public sealed class ElmaClientOptions
 
     public string BaseUrl { get; set; } = "https://elma.nass.usda.gov/";
     public string FoUpdateRequestPath { get; set; } = "FO/FOUpdateRequest.aspx";
-    public ElmaAuthenticationMode AuthenticationMode { get; set; } = ElmaAuthenticationMode.ApiKey;
+    public ElmaAuthenticationMode AuthenticationMode { get; set; } = ElmaAuthenticationMode.None;
     public string ApiKeyHeaderName { get; set; } = "X-API-Key";
     public string ApiKey { get; set; } = string.Empty;
     public string BearerToken { get; set; } = string.Empty;
